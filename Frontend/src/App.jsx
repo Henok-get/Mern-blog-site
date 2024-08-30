f47a6b0dae84842e8fc9873e4c2ed19654d0794f
Henok-get <hgetachew349@gmail.com>
2024-08-30 17:00:30 +0300

    Add pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,31 @@
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import about from './pages/about'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
 function App() {
   
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-red-50">
-      Hello world!
-    </h1>
-    </>
+ <BrowserRouter>
+ <Routes>
+<Route path="/" element={<Home/>}/>
+<Route path='/about' element={<about/>}/>
+<Route path='/sign-in' element={<Signin/>}/>
+<Route path='/sign-up' element={<Signup/>}/>
+
+<Route path='/dashboard' element={<Dashboard/>}/>
+
+<Route path='/projects' element={<Projects/>}/>
+
+
+
+
+ </Routes>
+ 
+ 
+ </BrowserRouter>
   )
 }
 
--- README.md
+++
@@ -1,2 +0,0 @@
-# Mern
-Mern stack project
